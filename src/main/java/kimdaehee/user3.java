37a390eca3b9168e7846edddd6bf73a732661e42
cjsrhd94 <cjsrhd94@gmail.com>
2022-01-20 14:50:28 +0900

    feat: delete something

--- src/main/java/kimdaehee/user3.java
+++ src/main/java/kimdaehee/user3.java
@@ -1,9 +1,6 @@
 package kimdaehee;
 
 public class user3 {
-    public String message() {
-        return "해줘 해줘 해줘333333333";
-    }
 
     public String user1() {
         return "333333333333";
@@ -15,11 +12,6 @@ public class user3 {
         return "333333333333";
     }
 
-
-
-    public String user3() {
-        return "333333333333";
-    }
 
 
 
