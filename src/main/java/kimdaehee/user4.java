4ecdf173a255111dc1cdf017393b65576bf7f940
kimdaehee90 <rotnprgl432@gmail.com>
2022-01-20 14:55:22 +0900

    faet: 수정 user4 444 삭제

--- src/main/java/kimdaehee/user4.java
+++ src/main/java/kimdaehee/user4.java
@@ -1,10 +1,10 @@
 package kimdaehee;
 
 public class user4 {
-    public String message(){
-        return "해줘 해줘 해줘44444444444;"}
+    public String message() {
+        return "해줘 해줘 해줘444444";
 //    public String user2(){return "4444444444444;"}
 //    public String user3(){return "4444444444444;"}
 
-
+    }
 }
